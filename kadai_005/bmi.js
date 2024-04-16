1568579ba48a2332d893e6dc4f20f0ff686d061a
hiroshi0512 <106784591+hiroshi0512@users.noreply.github.com>
2024-04-16 18:46:35 +0900

    課題：四則演算をしよう

---
+++ kadai_005/bmi.js
@@ -0,0 +1,14 @@
+// 変数の宣言
+let bodyWeightKG;
+let bodyHeightM;
+let bmi;
+
+// 値の代入
+bodyWeightKG = 68;
+bodyHeightM = 1.7;
+
+// BMIの算出
+bmi = bodyWeightKG / (bodyHeightM * bodyHeightM);
+
+// コンソールへの出力
+console.log(bmi);
